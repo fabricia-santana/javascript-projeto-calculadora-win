c8ef49bd78a9f51b4a681c4d2b703e7239c34c48
fabricia-santana <bricia_santana@hotmail.com>
2020-07-13 17:20:02 -0300

    Creating and validating and calculating the operations by moouse

--- controller/CalcController.js
+++ controller/CalcController.js
@@ -1,8 +1,12 @@
+
 class CalController{
 
     constructor(){
         this._display = document.querySelector("#display");
         this._operations = [];        
+        this._lastNumber = '';
+        this._lastOperator = '';
+        this._isOperatorClick= false;
         this.inicializer()
     }
 
@@ -42,18 +46,26 @@ class CalController{
             case 'C':
                 this.clearAll();
                 break;
-            case '%':                    
+            case 'X':
+                this.addOperation('*');
+                break;
+            case '÷':                                               
+                this.addOperation('/');
+                break;
+            case '%':  
+
             case '√':                
             case 'x²':                
             case '←':                
-            case '÷':                
-            case 'X':                   
+            
             case '±':                
             case ',':                
             case '=':                
             case '¹/x':   
             case '-':
             case '+':
+                this.addOperation(value);
+                break;
             case '0':
             case '1':
             case '2':
@@ -64,7 +76,7 @@ class CalController{
             case '7':
             case '8':
             case '9':
-                this.addOperation(value);
+                this.addOperation(parseFloat(value));
                 break;
             default:
                 this.setError();
@@ -78,26 +90,199 @@ class CalController{
     clearAll(){
         this.display = 0;
         this._operations = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
+        this._isOperatorClick= false;
     }
 
     setError(){
         this.display = 'ERROR';
     }
-
-    addOperation(value){ 
+    
+    addOperation(value){    
         
-      if(this._operations.length > 2){
-          if(this.isOperator(value)){
-            //calculate
-            console.log('É um operador');
-          }else{
-              this.pushOperations(value);
-          }
-      } else{
-          this.pushOperations(value);
-      }
-      
-      console.log(this._operations);
+        if(this.isOperator(value)){
+            this._isOperatorClick = true;
+            this.calculate(value);
+        }else{
+            this.pushNumber(value);
+        }
+        
+        console.log(this._operations);
+    }
+    
+    pushNumber(value){
+        if(this._operations.length === 0){
+            this._operations.push(value);
+            this._lastNumber = value;
+            
+        }else{
+            if(!this._isOperatorClick){
+                if(this._lastOperator !== '='){
+                    if(!isNaN(this.getLastPosition())){
+                        let newValue = parseFloat(this.getLastPosition().toString() + value.toString())
+                        this.setLastPosition(newValue);
+                        this._lastNumber = newValue;
+                        
+                    }else{
+                        this._operations.push(value);
+                        this._lastNumber = value;
+                        
+                    }
+                }else{
+                    this._operations = [value];
+                    this._lastOperator = '';
+                    this._isOperatorClick = false;
+                }
+            }else{
+                if(this.getLastPosition() === '='){
+                    this._operations = [value];
+                    
+                }else{
+                    this._operations.push(this._lastOperator);                
+                    this._operations.push(value);
+                    this._lastNumber = value;
+                    this._isOperatorClick = false;
+                }                
+            }
+        }
+        this.updateDisplay();
+    }
+    
+    calculate(value){ 
+
+        this._isOperatorClick = false;
+
+        if(value === '←'){
+            if(!this.isOperator(this.getLastPosition())){
+                let str = this.getLastPosition().toString();
+                if(str.length >1){
+                    this.setLastPosition(parseFloat(str.substring(0, str.length -1)));
+
+                } else{
+                    this.setLastPosition(0);
+                }
+                
+            }
+        }else if(value === '±'){
+            if(!isNaN(this.getLastPosition())){
+                if(Math.sign(this.getLastPosition() === -1)){
+                    this.setLastPosition(+this.getLastPosition());
+                }else{
+                    this.setLastPosition(-this.getLastPosition());
+
+                }
+            }
+        }
+        else{
+
+            if(this._operations.length === 0){
+    
+                if(value === '¹/x'){
+                    this.display = 'Cannot divide by zero';                
+                }else{
+                    this._operations.push(0);
+                    this._lastNumber = 0;
+                    this._operations.push(value);
+                    this._lastOperator = value;
+    
+                }
+    
+            }else if(this._operations.length === 1){
+                if(value === '='){
+                    if(this._lastOperator !== '' && this._lastOperator !== '¹/x' && this._lastOperator !== '√'){                    
+                        this._operations.push(this._lastOperator); 
+                        this._operations.push(this._lastNumber); 
+                        this._operations = [this.getResult()];                   
+                    } 
+                }else if(value === '¹/x'){
+                    if(this._operations.length > 0 && this.getLastPosition() !== 0  && !isNaN(this.getLastPosition())){
+                        this.calcDivisionX();
+                    }                  
+                }else if(value === 'x²'){
+                    this._operations.push('*');
+                    this._operations.push(this._lastNumber);
+                    this._operations = [this.getResult()];
+                    this._lastNumber = this._operations[0];
+                    this._lastOperator = '*';
+    
+                }else if(value === '√'){
+                    this._operations = [Math.sqrt(this._operations[0])];
+                    this._lastOperator = '√';
+                    this._lastNumber = this._operations[0];
+                    
+                }else{
+                    if(!this.isOperator(this.getLastPosition())){
+                        this._operations.push(value);
+                        this._lastOperator = value;
+    
+                    }else{
+                        this.setLastPosition(value);
+                        this._lastOperator = value;
+                    }
+                }
+                
+            }else if(this._operations.length === 2){
+                if(value === '='){
+                    this._operations.push(this._lastNumber);
+                    this._operations = [this.getResult()];
+                }else if (value === '%'){
+                    this._lastOperator = this._operations[1];
+                    this._lastNumber = this._operations[0];
+                    this._operations =  [this._operations[0] * this._operations[0]/100];
+                }
+                
+            }else if(this._operations.length === 3){
+                if(value === '='){
+                    console.log('Soma e operator Click true');
+                    this._operations = [this.getResult()];
+                    this._operations.push(value);
+                    this._isOperatorClick = true;
+                }else if(value === '%'){
+                    console.log('%');
+                }else{
+                    this._operations = [this.getResult()];
+                    if(value !== '='){
+                        this._operations.push(value);
+                        this._lastOperator = value;
+                    }
+                }
+            }
+        }
+        
+       this.updateDisplay();
+       console.log('Saiu calculate:'+ this._operations.length);
+        
+    }
+
+    calcSqrt(){
+        
+    }
+
+    calcDivisionX(){
+        let firstPosition = this._operations[0];
+        this._operations[0] = 1;
+        this._operations.push('/');
+        this._operations.push(firstPosition);
+        this._operations = [this.getResult()];
+        this._lastOperator = '¹/x';
+
+    }
+    
+
+    popOperations(){
+        if(!isNaN(this.getLastPosition())){
+            let value = this.getLastPosition().toString();
+            if(value.length > 1 && this._lastOperator === ''){
+                this._operations.pop();
+                value = value.slice(0, value.length - 1);
+                this._operations.push(parseFloat(value));
+                
+            }else{
+                this._operations.push(0);
+            }
+        }
+        this.updateDisplay();
     }
 
     getResult(){
@@ -106,55 +291,32 @@ class CalController{
 
    //**Verifica se o botão clicado é um operador * /
     isOperator(value){
-               
-        return (['+','-','%','√', 'x²','¹/x','←','÷','X','±','='].indexOf(value) > -1);
-    }
+              
+        return (['+','-','%','√', 'x²','¹/x','←','/','*','±','='].indexOf(value) > -1);
+    }   
 
     //**Obtem a última posição do array */
-    getLastOperator(){
+    getLastPosition(){
         return this._operations[this._operations.length - 1];
     }
 
-    setLastOperator(value){
+    setLastPosition(value){
         this._operations[this._operations.length - 1] = value;
     }         
-    
-    pushOperations(value){
-        if(this._operations.length !== 0){
 
-            if(isNaN(this.getLastOperator())){
-              
-               if(this.isOperator(value)){
-                   this.setLastOperator(value);
-               }else{
-                   this._operations.push(value);
-                   this.display = this.getLastOperator();
-               }
-            }else{            
-                if(!isNaN(value)){
-                    let newValue = this.getLastOperator() + value;
-                    this.setLastOperator(newValue);
-                    this.display = newValue;
-                }else{
-                    this._operations.push(value);                    
-                }
+    updateDisplay(){
+        if(this._operations.length > 0){
+            if(this.isOperator(this.getLastPosition())){
+                this.display = this._operations[this._operations.length - 2];
+            }else{
+                this.display = this.getLastPosition();
             }
-          }else{
-              
-              if(this.isOperator(value)){
-                  if(value === '¹/x'){
-                      this.display = 'Cannot divide by zero';
-                  }else{
-                      this._operations.push(0);
-                      this._operations.push(value);                        
-                }
-                  
-              }else{
-                this.display = value;
-                this._operations.push(value)
-              }
-          }
+
+        }else{
+            this.display = '0';
+        }
 
     }
-    
+
+   
 }
